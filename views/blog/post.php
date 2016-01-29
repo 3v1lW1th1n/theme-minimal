2717bdb0dbe4316d156706551842d3e379463908
Sascha Dube <sascha.dube@gmail.com>
2016-01-29 16:38:38 +0100

    cleanup

--- views/blog/post.php
+++ views/blog/post.php
@@ -9,17 +9,18 @@
 
     ?>
     <div class="tm-article-border">
+
         <h1 class="uk-article-title uk-margin-small-bottom <?= ($params['blog_alignment']) ? 'uk-text-center' : '' ?>"><?= $post->title ?></h1>
 
         <p class="uk-article-meta uk-margin-small-top <?= ($params['blog_alignment']) ? 'uk-text-center' : '' ?>">
             <time datetime="<?=$post->date->format(\DateTime::W3C)?>" v-cloak>{{ "<?=$post->date->format(\DateTime::W3C)?>" | date "longDate" }}</time>
         </p>
+
     </div>
 
     <?php if ($image = $post->get('image.src')): ?>
     <img src="<?= $image ?>" alt="<?= $post->get('image.alt') ?>">
     <?php endif ?>
-
 
     <div class="uk-margin-large-top tm-container-mini">
 
