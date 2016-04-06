181b21d0d9a6aae8529675af14992b7a8097e1b0
Sascha Dube <sascha.dube@gmail.com>
2016-04-06 18:10:42 +0200

    better option naming

--- app/components/node-theme.vue
+++ app/components/node-theme.vue
@@ -40,7 +40,7 @@
         </div>
 
         <div class="uk-form-row">
-            <label for="form-top-style" class="uk-form-label">{{ 'Top Style' | trans }}</label>
+            <label for="form-top-style" class="uk-form-label">Top {{ 'Position' | trans }}</label>
             <div class="uk-form-controls">
                 <select id="form-top-style" class="uk-form-width-large" v-model="node.theme.top_style">
                     <option value="uk-block-default">{{ 'Default' | trans }}</option>
@@ -50,7 +50,7 @@
         </div>
 
         <div class="uk-form-row">
-            <label for="form-main-style" class="uk-form-label">{{ 'Main Style' | trans }}</label>
+            <label for="form-main-style" class="uk-form-label">Main {{ 'Position' | trans }}</label>
             <div class="uk-form-controls">
                 <select id="form-main-style" class="uk-form-width-large" v-model="node.theme.main_style">
                     <option value="uk-block-default">{{ 'Default' | trans }}</option>
@@ -60,7 +60,7 @@
         </div>
 
         <div class="uk-form-row">
-            <label for="form-bottom-style" class="uk-form-label">{{ 'Bottom Style' | trans }}</label>
+            <label for="form-bottom-style" class="uk-form-label">Bottom {{ 'Position' | trans }}</label>
             <div class="uk-form-controls">
                 <select id="form-bottom-style" class="uk-form-width-large" v-model="node.theme.bottom_style">
                     <option value="uk-block-default">{{ 'Default' | trans }}</option>
@@ -70,12 +70,13 @@
         </div>
 
         <div class="uk-form-row">
-            <label for="form-footer-style" class="uk-form-label">{{ 'Footer Style' | trans }}</label>
+            <label for="form-footer-style" class="uk-form-label">Footer {{ 'Position' | trans }}</label>
             <div class="uk-form-controls">
                 <select id="form-footer-style" class="uk-form-width-large" v-model="node.theme.footer_style">
                     <option value="uk-block-default">{{ 'Default' | trans }}</option>
                     <option value="uk-block-muted">{{ 'Muted' | trans }}</option>
                 </select>
+                <p class="uk-form-help-block uk-margin">{{ 'Note: Position settings only work if you publish a widget in that position.' | trans }}</p>
             </div>
         </div>
 
